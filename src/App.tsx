343b7fca500c7baf497ab27fc4d7ead4756fcabb
ProgrammingHero1 <nusratjahannila2122232425@gmail.com>
2026-09-11 19:07:49 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Banner from './Components/Banner'
+import Navbar from './Components/Navbar'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Logo from '../assets/logo-text.png'
+import { IoMenuSharp } from "react-icons/io5";
+
+
+const Navbar = () => {
+    return (
+        <nav className='w-full md:w-[1425px] body flex justify-between items-center'>
+            <div className='flex items-center gap-34'>
+                <button className="md:hidden">
+                    <IoMenuSharp className='text-3xl' />
+                </button>
+                <img src={Logo}alt="" />
+            </div>
+            <div className="hidden md:flex list-none items-center font-medium text-[1rem] text-[#475569] gap-8">
+                
+                <li className='text-[#DB2777]'>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </div>
+            <div className='flex items-center gap-3'>
+                <h2>Sign In</h2>
+                <button className="btn btn-active btn-secondary  rounded-[55px]">Sign Up</button>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
